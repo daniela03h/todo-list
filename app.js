371be771317f9221f322e7a418554e93f7ed9680
daniela03h <daniela0393h@gmail.com>
2022-03-15 18:17:57 -0500

    v1.4 - Se puede agregar tareas, marcar y desmarcarlas

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let $taskCounter = document.querySelector("#contador-tareas-pendientes")
 let $newTaskForm = document.querySelector(".new-task-form")
 let $resertFormButton = document.querySelector(".clear-button")
 let $tasksList = document.querySelector(".tasks-list")
+let $newTask = document.querySelector(".caja-de-texto")
+
+$newTaskForm.addEventListener("submit", addTask)
 
 let todos = [
   {
@@ -15,19 +18,56 @@ let todos = [
   {
     id: 1646783819675,
     description: "Practicas mucho JavaScript",
-    isDone: false
+    isDone: true
 
   }
 
 ];
 
+function addTask(event) {
+  event.preventDefault()
+
+  if ($newTask.value === "") {
+    return // Detenemos la ejecucion de la funcion
+  }
+
+  let newTask = {
+    id: new Date().getTime(),
+    description: $newTask.value, //$newTask: referencia al input de HTML
+    isDone: false
+  }
+
+  todos.push(newTask)
+  renderTodos()
+  $newTask.value = ""
+}
+
+function checkTask(posicion) {
+  todos[posicion].isDone = todos[posicion].isDone === true ? false : true
+  renderTodos()
+}
+
+function renderPendingTasks() {
+  const pendingsTasks = todos.filter(function (task) {
+    const noEstaTerminada = task.isDone == false
+    return noEstaTerminada
+  })
+
+  const caunterPendingTasks = pendingsTasks.length
+  $taskCounter.innerHTML = `${caunterPendingTasks} ${caunterPendingTasks > 1 ? "Tareas Pendientes" : "Tarea Pendiente"}`
+
+}
+
 function renderTodos() {
+  renderPendingTasks()
   $tasksList.innerHTML = ""
   let renderedTasks = todos.map(function (task) {
-    return`
+    return `
     <li class="task-list-item">
         <button class="button-list check-task-button">
-          <i class="fa-regular fa-circle-check"></i>
+          <i 
+          class="fa-regular fa-circle-check ${task.isDone === true ? "fa-circle-check-done" : ""}"
+          ></i>
         </button>
         <span class="task-description">
         ${task.description}
@@ -38,6 +78,12 @@ function renderTodos() {
       </li> `
   })
   $tasksList.innerHTML = renderedTasks.join("")
+
+  const $checkButtons = document.querySelectorAll(".check-task-button")
+  $checkButtons.forEach(function ($checkButton, posicion) {
+    $checkButton.addEventListener("click", function(){ 
+      checkTask(posicion)})
+  })
 }
 
 renderTodos()
