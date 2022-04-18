c9960ead1cc3d2cf9cec269d05d09b9172f0e08a
daniela03h <daniela0393h@gmail.com>
2022-04-18 16:57:55 -0500

    v2.0 - Todo list finalizado

--- app.js
+++ app.js
@@ -1,72 +1,114 @@
-console.log("Module loaded!")
-let $tituloPrincipal = document.querySelector("#titulo-ppal")
-let $taskCounter = document.querySelector("#contador-tareas-pendientes")
-let $newTaskForm = document.querySelector(".new-task-form")
-let $resertFormButton = document.querySelector(".clear-button")
-let $tasksList = document.querySelector(".tasks-list")
-let $newTask = document.querySelector(".caja-de-texto")
+console.log('Module loaded!');
+let todos = [];
+let $tituloPrincipal = document.querySelector('#titulo-ppal');
+let $taskCounter = document.querySelector('#contador-tareas-pendientes');
+let $newTaskForm = document.querySelector('.new-task-form');
+let $resertFormButton = document.querySelector('.clear-button');
+let $tasksList = document.querySelector('.tasks-list');
+let $newTask = document.querySelector('.caja-de-texto');
 
-$newTaskForm.addEventListener("submit", addTask)
+window.addEventListener('load', function () {
+  const savedTodos = JSON.parse(window.localStorage.getItem('todos'));
+  todos = savedTodos || []; // en caso de que saved todos sea null o undefined ponemos por defecto un array vacio
+  renderDate();
+  renderTodos();
+});
 
-let todos = [
-  {
-    id: 1646783819670,
-    description: "Practicas mucho JavaScript",
-    isDone: false
-
-  },
-  {
-    id: 1646783819675,
-    description: "Practicas mucho JavaScript",
-    isDone: true
-
-  }
-
-];
+$newTaskForm.addEventListener('submit', addTask);
 
 function addTask(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  if ($newTask.value === "") {
-    return // Detenemos la ejecucion de la funcion
+  if ($newTask.value === '') {
+    return; // Detenemos la ejecucion de la funcion
   }
 
   let newTask = {
     id: new Date().getTime(),
     description: $newTask.value, //$newTask: referencia al input de HTML
-    isDone: false
-  }
+    isDone: false,
+  };
 
-  todos.push(newTask)
-  renderTodos()
-  $newTask.value = ""
+  todos.push(newTask);
+  saveTodos();
+  renderTodos();
+  $newTask.value = '';
 }
 
 function checkTask(posicion) {
-  todos[posicion].isDone = todos[posicion].isDone === true ? false : true
-  renderTodos()
+  todos[posicion].isDone = todos[posicion].isDone === true ? false : true;
+  saveTodos();
+  renderTodos();
+}
+
+function removeTask(posicion) {
+  todos = todos.filter(function (_, indice) {
+    return indice !== posicion;
+  });
+  saveTodos();
+  renderTodos();
+}
+
+function renderDate() {
+  const daysOfWeek = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+  ];
+  const months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
+  const date = new Date();
+  const day = date.getDate();
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  $tituloPrincipal.innerHTML = `${dayOfWeek}, ${day} ${month} ${year}`;
 }
 
 function renderPendingTasks() {
   const pendingsTasks = todos.filter(function (task) {
-    const noEstaTerminada = task.isDone == false
-    return noEstaTerminada
-  })
+    const noEstaTerminada = task.isDone == false;
+    return noEstaTerminada;
+  });
 
-  const caunterPendingTasks = pendingsTasks.length
-  $taskCounter.innerHTML = `${caunterPendingTasks} ${caunterPendingTasks > 1 ? "Tareas Pendientes" : "Tarea Pendiente"}`
+  const caunterPendingTasks = pendingsTasks.length;
+  $taskCounter.innerHTML = `${caunterPendingTasks} ${
+    caunterPendingTasks > 1 ? 'Tareas Pendientes' : 'Tarea Pendiente'
+  }`;
+}
 
+function saveTodos() {
+  window.localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 function renderTodos() {
-  renderPendingTasks()
-  $tasksList.innerHTML = ""
+  renderPendingTasks();
+  $tasksList.innerHTML = '';
   let renderedTasks = todos.map(function (task) {
     return `
     <li class="task-list-item">
         <button class="button-list check-task-button">
           <i 
-          class="fa-regular fa-circle-check ${task.isDone === true ? "fa-circle-check-done" : ""}"
+          class="fa-regular fa-circle-check ${
+            task.isDone === true ? 'fa-circle-check-done' : ''
+          }"
           ></i>
         </button>
         <span class="task-description">
@@ -75,15 +117,21 @@ function renderTodos() {
         <button type="button" class="button-list remove-task-button">
           <i class="fa-regular fa-trash-can"></i>
         </button>
-      </li> `
-  })
-  $tasksList.innerHTML = renderedTasks.join("")
+      </li> `;
+  });
+  $tasksList.innerHTML = renderedTasks.join('');
 
-  const $checkButtons = document.querySelectorAll(".check-task-button")
+  const $checkButtons = document.querySelectorAll('.check-task-button');
   $checkButtons.forEach(function ($checkButton, posicion) {
-    $checkButton.addEventListener("click", function(){ 
-      checkTask(posicion)})
-  })
-}
+    $checkButton.addEventListener('click', function () {
+      checkTask(posicion);
+    });
+  });
 
-renderTodos()
+  const $removeButtons = document.querySelectorAll('.remove-task-button');
+  $removeButtons.forEach(function ($removeButton, posicion) {
+    $removeButton.addEventListener('click', function () {
+      removeTask(posicion);
+    });
+  });
+}
